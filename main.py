977c676d54705789bf3206adcf3042960141073a
f4d3 <josew.asdf@gmail.com>
2020-05-08 01:16:50 -0400

    Adding the -f (follow) and default all linemessage

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ import telegram
 def check_args():
     parser = argparse.ArgumentParser(description = "Simple Shout-it-out telegram notificator")
     parser.add_argument('-c', '--config',help="Full path to config file (default is ~/.SIO.conf")
-    parser.add_argument('-f', '--file',help="Send a text file (default is stdin)")
+    parser.add_argument('-i', '--infile',help="Send a text file (default is stdin)")
     parser.add_argument('-v', '--verbose',required = False, help="Turn on the verbose mode", action="store_true",default=False)
     parser.add_argument('-F', '--filter',required = False, help="Add a filter before sending the message (string: default: None)",default=False)
+    parser.add_argument('-f', '--follow', required=False,
+                        help="Send one line at a time", action="store_true", default=False)
     return vars(parser.parse_args())
 
 
@@ -37,7 +39,7 @@ def send_message_byfile(bot, file_name,chat_id = ""):
             bot.send_message(chat_id = chat_id, text = str(k))
     return True
 
-def send_message_default(bot,args,chat_id = ""):
+def send_message_follow(bot,args,chat_id = ""):
     #chat_id = get_chat_id(bot)
     line_list = []
     verbose = args['verbose']
@@ -54,13 +56,16 @@ def send_message_default(bot,args,chat_id = ""):
                     pass
                 if verbose:
                     print("Sending: {}".format( str(line) ) )
+                line = """`{}`""".format(line)
                 if args['filter'] != False:
                     if verbose:
                         print("Using filter: '{}'...".format(args['filter']))
                     if args['filter'] in line:
-                        bot.send_message(chat_id = chat_id, text = str(line))
+                        bot.send_message(chat_id=chat_id, text=str(
+                            line), parse_mode=telegram.ParseMode.MARKDOWN)
                     continue
-                bot.send_message(chat_id = chat_id, text = str(line))
+                bot.send_message(chat_id=chat_id, text=str(
+                    line), parse_mode=telegram.ParseMode.MARKDOWN)
     except KeyboardInterrupt:
         print("Exiting...")
         exit(0)
@@ -68,6 +73,33 @@ def send_message_default(bot,args,chat_id = ""):
     #    if line != "":
     #        bot.send_message(chat_id = chat_id, text = str(line))
     return True
+
+
+def send_message_default(bot, args, chat_id = ""):
+    verbose = args['verbose']
+    
+    if verbose: print(args)
+    whole_message = "```\n"
+    # Get the chat id
+    try:
+        new_chat_id = get_chat_id(bot)
+        chat_id = new_chat_id
+    except Exception as e:
+        if verbose: print("Error getting the chat_id, using {} from config file".format(chat_id))
+        pass
+    try:
+        for line in sys.stdin:
+            whole_message += line
+    except Exception as e:
+        whole_message = "Error with {}".format(e)
+        print(whole_message)
+        return False
+    whole_message += "\n```"
+    if verbose:
+        print("Sending the following message: \n{}".format(whole_message))
+    bot.send_message(chat_id = chat_id,text = whole_message, parse_mode=telegram.ParseMode.MARKDOWN)
+    return True
+    
 
 def write_chatid_2config(chat_id, config, path):
     config['DEFAULT']['saved_chat_id'] = str(chat_id)
@@ -84,6 +116,7 @@ if __name__ == "__main__":
         config_location = args['config']
     config.read(config_location)
     if args['verbose'] == True: print("Reading the config from {}".format(config_location))
+    if args['verbose'] == True: print(args)
     # Get the config and api key
     bot_api_key = config['DEFAULT']['apikey']
     if args['verbose'] == True: print("using Api key {}".format(bot_api_key))
@@ -102,10 +135,15 @@ if __name__ == "__main__":
         if args['verbose']: print("Chat id saved on config file!")
 
 
-    if args['file'] == None:
-        send_message_default(bot,args,chat_id)
-        exit(0)
-    send_message_byfile(bot,args['file'],chat_id)
+    if args['follow']:
+        send_message_follow(bot, args, chat_id)
+        sys.exit(0)
+    
+    elif not args['follow'] and args['infile'] == None:
+        send_message_default(bot, args, chat_id)
+        sys.exit(0)
+
+    send_message_byfile(bot,args['infile'],chat_id)
 
 
 
